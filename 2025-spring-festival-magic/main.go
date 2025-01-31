491cb669b7f987ae580d4dac270edc04489cb3b9
江湖十年 <jianghushinian007@163.com>
2025-01-31 11:38:44 +0800

    Update 2025-spring-festival-magic/main.go

--- 2025-spring-festival-magic/main.go
+++ 2025-spring-festival-magic/main.go
@@ -21,7 +21,10 @@ func permute(items []string, start int) {
 	}
 }
 
+// NOTE: 经网友提醒，这个是错的的实现，我发现确实写错了，不过还是保留在这里供大家参考
+
 // 魔术
+/*
 func magic(items []string) {
 	old := make([]string, len(items))
 	copy(old, items)
@@ -49,6 +52,42 @@ func magic(items []string) {
 		if items[i] == "勺子🥄" {
 			// 勺子如果不在最左边，交换到最左边
 			items[i], items[0] = items[0], items[i]
+			break
+		}
+	}
+
+	// 打印当前和经过魔术操作后的排列
+	fmt.Println("当前排列：", old, " => ", "魔术操作后：", items)
+}
+*/
+
+// NOTE: 下面这个函数才是正确的实现
+
+// 魔术
+func magic(items []string) {
+	old := make([]string, len(items))
+	copy(old, items)
+
+	// 1. 筷子跟它左边的物品互换，如果筷子已经在最左边，则无需移动
+	for i := 1; i < len(items); i++ {
+		if items[i] == "筷子🥢" {
+			items[i-1], items[i] = items[i], items[i-1]
+			break
+		}
+	}
+
+	// 2. 杯子跟它右边的物品互换，如果杯子已经在最右边，则无需移动
+	for i := len(items) - 2; i >= 0; i-- {
+		if items[i] == "杯子🍺" {
+			items[i], items[i+1] = items[i+1], items[i]
+			break
+		}
+	}
+
+	// 3. 勺子跟它左边的物品互换，如果勺子已经在最左边，则无需移动
+	for i := 1; i < len(items); i++ {
+		if items[i] == "勺子🥄" {
+			items[i-1], items[i] = items[i], items[i-1]
 			break
 		}
 	}
